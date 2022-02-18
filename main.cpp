c9c1e40378f5671724fd6f69a8226b0a0cd50449
minhngo <tngo238@student.cccd.edu>
2022-02-17 21:47:26 -0800

    Update capacity of array

--- main.cpp
+++ main.cpp
@@ -3,16 +3,13 @@
 // @date: 01/18/2022
 
 #include "interface.h"
-#include "interface.cpp"
 #include "ai.h"
-#include "ai.cpp"
+
 
 int main() 
 {
-
     // Generate empty tictactoe Board
-    int num = SIZE_OF_BOARD;
-    char tictactoe[SIZE_OF_BOARD][SUB_SIZE_OF_BOARD];    
+    char tictactoe[3][3];    
     resetGame(tictactoe);
     runGame(tictactoe);
 }
